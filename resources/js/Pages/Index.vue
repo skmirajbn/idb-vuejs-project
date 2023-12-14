9d36a19d5e96ba399b5cad5ec25335e0810690cc
skmirajbn <skmirajbn@gmail.com>
2023-12-14 10:54:02 +0600

    updated

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,4 +1,13 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+import { Head } from "@inertiajs/vue3";
+
+defineProps({});
+</script>
+
 <template>
+    <Head title="Home"></Head>
     <div class="flex mx-auto max-w-7xl">
         <div class="w-1/4 text-4xl bg-green-400">Logo</div>
         <div class="w-2/4 text-4xl bg-green-400">
@@ -7,12 +16,5 @@
         <div class="w-1/4 text-4xl bg-green-400">Search</div>
     </div>
 </template>
-
-<script setup>
-// import link compoent from intertia vue
-import { Link } from "@inertiajs/vue3";
-
-defineProps({});
-</script>
 
 <style scoped></style>
