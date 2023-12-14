632b62ce6ac67b3b53781c05c575866a370e3d26
skmirajbn <skmirajbn@gmail.com>
2023-12-14 22:25:53 +0600

    updated

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,26 +1,90 @@
 <template>
     <div class="min-h-screen">
-        <header class="bg-info">
-            <div class="container flex mx-auto">
-                <div class="w-1/4 text-2xl">
+        <header class="bg-emerald-500">
+            <div class="container flex items-center py-2 mx-auto">
+                <div class="w-1/6 text-2xl">
                     <Link href="/login">Logo</Link>
                 </div>
-                <div class="w-2/4 text-4xl">
+                <div class="flex items-center w-4/6 gap-3">
                     <Link href="/">
-                        <button class="btn btn-primary btn-md">Home</button>
+                        <button class="btn btn-primary btn-sm">Home</button>
                     </Link>
                     <Link href="/about">
-                        <button class="btn btn-primary btn-md">About</button>
+                        <button class="btn btn-primary btn-sm">About</button>
                     </Link>
                     <Link href="/">
-                        <button class="btn btn-primary btn-md">Home</button>
+                        <button class="btn btn-primary btn-sm">Home</button>
                     </Link>
+                    <a href="https://laravelvueidb.skmiraj.online">
+                        <button class="btn btn-primary btn-sm">
+                            Live Link
+                        </button>
+                    </a>
                 </div>
-                <div class="w-1/4 text-4xl">Search</div>
+                <div class="flex items-center justify-end w-1/6 gap-3">
+                    <div class="avatar">
+                        <div class="w-10 rounded-full">
+                            <img
+                                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                            />
+                        </div>
+                    </div>
+                    <details class="dropdown">
+                        <summary class="btn btn-sm btn-primary">
+                            Sk Miraj
+                            <i class="fa-solid fa-angle-down"></i>
+                        </summary>
+                        <ul
+                            class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52"
+                        >
+                            <li><a>Profile</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </details>
+                </div>
             </div>
         </header>
-        <slot />
-        <footer class="py-2 bg-secondary">footer</footer>
+        <div class="container py-6 mx-auto"><slot /></div>
+        <footer class="p-10 footer bg-base-200 text-base-content">
+            <nav>
+                <header class="footer-title">Services</header>
+                <a class="link link-hover">Branding</a>
+                <a class="link link-hover">Design</a>
+                <a class="link link-hover">Marketing</a>
+                <a class="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <header class="footer-title">Company</header>
+                <a class="link link-hover">About us</a>
+                <a class="link link-hover">Contact</a>
+                <a class="link link-hover">Jobs</a>
+                <a class="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <header class="footer-title">Legal</header>
+                <a class="link link-hover">Terms of use</a>
+                <a class="link link-hover">Privacy policy</a>
+                <a class="link link-hover">Cookie policy</a>
+            </nav>
+            <form>
+                <header class="footer-title">Newsletter</header>
+                <fieldset class="form-control w-80">
+                    <label class="label">
+                        <span class="label-text">Enter your email address</span>
+                    </label>
+                    <div class="join">
+                        <input
+                            type="text"
+                            placeholder="username@site.com"
+                            class="input input-bordered join-item"
+                        />
+                        <button class="btn btn-primary join-item">
+                            Subscribe
+                        </button>
+                    </div>
+                </fieldset>
+            </form>
+        </footer>
     </div>
 </template>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,8 @@ createInertiaApp({
             .mount(el);
     },
     progress: {
-        color: "#4B5563",
+        color: "#EA580C",
+        includeCSS: true,
+        showSpinner: true,
     },
 });
