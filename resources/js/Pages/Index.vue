7443db519ad4e8fb019d8c51d286fad7eae6090d
skmirajbn <skmirajbn@gmail.com>
2023-12-14 10:07:25 +0600

    updated

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -2,7 +2,7 @@
     <div class="flex mx-auto max-w-7xl">
         <div class="w-1/4 text-4xl bg-green-400">Logo</div>
         <div class="w-2/4 text-4xl bg-green-400">
-            <Link :href="route('login')">Login cmd testa</Link>
+            <Link :href="route('login')">Ssh restarted</Link>
         </div>
         <div class="w-1/4 text-4xl bg-green-400">Search</div>
     </div>
