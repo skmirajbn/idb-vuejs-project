79484fe49dbf63378e94e46c21e8ef9e09141dea
skmirajbn <skmirajbn@gmail.com>
2023-12-25 23:14:17 +0600

    updated

---
+++ resources/js/Pages/Admin/Products/Create.vue
@@ -0,0 +1,78 @@
+<template>
+    <AdminLayout>
+        <div class="w-full p-4 space-y-4 bg-gray-200 rounded-xl">
+            <div class="flex justify-between">
+                <h3 class="text-2xl font-bold">
+                    <i class="fa-solid fa-building-circle-arrow-right"></i> Add
+                    District
+                </h3>
+                <Link :href="route('admin.district.index')"
+                    ><button class="btn btn-primary">
+                        All Districts
+                    </button></Link
+                >
+            </div>
+            <div class="py-10">
+                <form
+                    class="flex flex-col gap-4"
+                    @submit.prevent="handleSubmit"
+                >
+                    <div class="flex flex-col gap-2">
+                        <label class="text-xl font-bold" for="name">Name</label>
+                        <input
+                            class="rounded-lg"
+                            id="name"
+                            type="text"
+                            v-model="form.district"
+                        />
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.district"
+                        />
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">
+                        Add District
+                    </button>
+                </form>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+import InputError from "@/Components/InputError.vue";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import { Link, useForm } from "@inertiajs/vue3";
+import { inject } from "vue";
+
+const swal = inject("$swal");
+
+const form = useForm({
+    district: "",
+    remember: false,
+});
+const handleSubmit = () => {
+    console.log(form);
+    form.post(route("admin.district.store"), {
+        preserveState: true,
+        onFinish: () => form.reset(),
+        onSuccess: (data) => {
+            form.reset();
+            swal({
+                icon: "success",
+                title: "District Added",
+                text: data.props.message,
+            });
+        },
+        onError: (err) => {
+            console.log(err);
+            swal({
+                icon: "error",
+                title: "Oops...",
+                text: err.district,
+            });
+        },
+    });
+};
+</script>
---
+++ resources/js/Pages/Admin/Products/Index.vue
@@ -0,0 +1,77 @@
+<template>
+    <AdminLayout>
+        <div class="w-full p-4 space-y-4 bg-gray-200 rounded-xl">
+            <div class="flex justify-between">
+                <h3 class="text-2xl font-bold">
+                    <i class="fa-solid fa-building-circle-arrow-right"></i> All
+                    Districts
+                </h3>
+                <Link :href="route('admin.district.create')">
+                    <button class="block btn btn-primary">
+                        Add New <i class="fa-solid fa-plus"></i>
+                    </button>
+                </Link>
+            </div>
+            <div class="overflow-x-auto">
+                <table class="table">
+                    <!-- head -->
+                    <thead class="text-xl text-white bg-emerald-600">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-lg">
+                        <tr v-for="district in districts">
+                            <th>{{ district.id }}</th>
+                            <td class="font-bold">{{ district.name }}</td>
+                            <td class="flex gap-2">
+                                <Link
+                                    :href="
+                                        route('admin.district.edit', {
+                                            district: district.id,
+                                        })
+                                    "
+                                >
+                                    <button class="btn btn-primary">
+                                        Edit
+                                    </button></Link
+                                >
+                                <button
+                                    class="btn btn-warning"
+                                    @click="deleteDistrict(district.id)"
+                                >
+                                    Delete
+                                </button>
+                            </td>
+                        </tr>
+                        <!-- row 3 -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </AdminLayout>
+</template>
+
+<script setup>
+import AdminLayout from "@/Layouts/AdminLayout.vue";
+import { Link, router } from "@inertiajs/vue3";
+import { inject } from "vue";
+defineProps({
+    districts: Object,
+});
+const swal = inject("$swal");
+const deleteDistrict = (id) => {
+    router.visit(route("admin.district.destroy", id), {
+        method: "delete",
+        onSuccess: () => {
+            swal({
+                icon: "success",
+                title: "District Deleted",
+                text: "District Deleted Successfully",
+            });
+        },
+    });
+};
+</script>
